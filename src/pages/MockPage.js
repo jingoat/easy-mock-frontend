f5c69182c21dd55df205756ccde68690fdda5fa1
jingoat <806128271@qq.com>
2021-12-03 00:09:39 +0800

    feature: easy-mock-frontend crud

--- src/pages/MockPage.js
+++ src/pages/MockPage.js
@@ -1,15 +1,33 @@
 import React, { useEffect, useState, useRef } from "react";
+import { Button, Input } from "antd";
 import "./MockPage.style.css";
+
+const { TextArea } = Input;
 
 function MockPage() {
   useEffect(() => {}, []);
 
   return (
     <div className="mock-page">
-      <div className="mock-top">1</div>
+      <div className="mock-top">
+        <Input className="search-input" />
+        <Button type="primary" className="search-btn mock-top-btn">
+          搜索
+        </Button>
+        <Button type="primary" className="add-btn mock-top-btn">
+          添加
+        </Button>
+      </div>
       <div className="mock-bottom">
-        <div className="mock-bottom-left mock-bottom-item">2</div>
-        <div className="mock-bottom-right mock-bottom-item">3</div>
+        <div className="mock-bottom-left mock-bottom-item">
+          <div className="data-item">/test1</div>
+          <div className="data-item">/test1/test2</div>
+          <div className="data-item">/test/test2/test3</div>
+          <div className="data-item">/test/test2/test3/test4</div>
+        </div>
+        <div className="mock-bottom-right mock-bottom-item">
+          <TextArea rows={100} />
+        </div>
       </div>
     </div>
   );
