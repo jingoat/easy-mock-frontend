65b231161847a9bdc0690cfa9547f52c4e025965
jingoat <806128271@qq.com>
2021-12-04 11:56:52 +0800

    feature: easy-mock-frontend crud

--- src/pages/MockPage.js
+++ src/pages/MockPage.js
@@ -20,13 +20,19 @@ function MockPage() {
       </div>
       <div className="mock-bottom">
         <div className="mock-bottom-left mock-bottom-item">
-          <div className="data-item">/test1</div>
-          <div className="data-item">/test1/test2</div>
-          <div className="data-item">/test/test2/test3</div>
-          <div className="data-item">/test/test2/test3/test4</div>
+          <a className="data-item">/test1</a>
+          <a className="data-item">/test1/test2</a>
+          <a className="data-item">/test/test2/test3</a>
+          <a className="data-item">/test/test2/test3/test4</a>
         </div>
         <div className="mock-bottom-right mock-bottom-item">
-          <TextArea rows={100} />
+          <Button type="primary" className="update-btn mock-top-btn">
+            保存
+          </Button>
+          <Button type="primary" className="delete-btn mock-top-btn">
+            删除
+          </Button>
+          <TextArea rows={60} className="text-area" />
         </div>
       </div>
     </div>
